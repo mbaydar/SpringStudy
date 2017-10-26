2a32e5e8379fc551d1fad4650138e906eb0fb851
mucahitbaydar <mucahitbaydar@gmail.com>
2017-10-26 21:51:00 +0300

    Inner Beans, Aliases

--- SpringDemo/src/com/baydar/DrawingApp.java
+++ SpringDemo/src/com/baydar/DrawingApp.java
@@ -13,7 +13,7 @@ public class DrawingApp {
 //		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));	
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		Triangle tr = (Triangle) context.getBean("triangle");
+		Triangle tr = (Triangle) context.getBean("triangle-alias");
 		
 //		Triangle tr = new Triangle();
 		tr.draw();
