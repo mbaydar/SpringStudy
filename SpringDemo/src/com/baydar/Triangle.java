70b991b8d8d52de667690c04c42339b582864d7d
mucahitbaydar <mucahitbaydar@gmail.com>
2017-10-26 21:28:14 +0300

    Injecting Objects

--- SpringDemo/src/com/baydar/Triangle.java
+++ SpringDemo/src/com/baydar/Triangle.java
@@ -2,35 +2,46 @@ package com.baydar;
 
 public class Triangle {
 
-	private String type;
-	private int height;
-	
-	public int getHeight() {
-		return height;
-	}
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 
-	public Triangle(String type) {
-		this.type = type;
-	}
-	
-	public Triangle(int height) {
-		this.height = height;
-	}
-	
-	public Triangle(String type, int height) {
-		this.type = type;
-		this.height = height;
-	}
-	
-	public String getType() {
-		return type;
-	}
 
 //	public void setType(String type) {
 //		this.type = type;
 //	}
 
 	public void draw() {
-		System.out.println(getType() + " Triangle drawn of height " + getHeight());
+		System.out.println(getPointA() + " " + getPointB() + " " + getPointC());
+	}
+
+
+	public Point getPointA() {
+		return pointA;
+	}
+
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 }
